67c6ccaae9ae5a4bfea5f8daa8eda4c1af3cf7c6
DatThanh411 <111189800+datnguyen2000@users.noreply.github.com>
2022-08-21 00:15:33 +0700

    Fix bug index

--- js/script.js
+++ js/script.js
@@ -18,6 +18,35 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+  $('.new-game-item').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      prevArrow: `<button type='button' class='slick-prev slick-arrow'> <i class="fa-solid fa-circle-chevron-left"></i></button>`,
+      nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-circle-chevron-right"></i></button>`,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+    ]
+  });
+});
+
 $('.navbar-toggler').click(function(){
   var icon = $('.header-icon');
   console.log(icon[0]);
