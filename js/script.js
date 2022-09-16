1c570f3d770969370ef9473d2d0a1a7da4bd31a2
DatThanh411 <111189800+datnguyen2000@users.noreply.github.com>
2022-09-17 01:16:06 +0700

    update

--- js/script.js
+++ js/script.js
@@ -79,6 +79,52 @@ $(function () {
   });
 });
 
+
+function hideRight() {
+  var right = document.getElementsByClassName('right-menu');
+
+  right[0].style = "right:-300px; transition: right .3s;";
+}
+
+function unhideRight() {
+  var right = document.getElementsByClassName('right-menu');
+
+  right[0].style = "right:0; transition: right .3s;";
+}
+
+
+function login() {
+  var login = document.getElementsByClassName('login-form');
+
+  var register = document.getElementsByClassName('form-register');
+
+  var border = document.getElementsByClassName('register');
+
+  var borderLogin = document.getElementsByClassName('login');
+
+  borderLogin[0].style = "border-bottom: 3.5px solid red;";
+  border[0].style = "border:none;";
+
+  login[0].style = "display:block;";
+  register[0].style = "display:none;";
+}
+
+function register() {
+  var login = document.getElementsByClassName('login-form');
+
+  var register = document.getElementsByClassName('form-register');
+
+  var border = document.getElementsByClassName('register');
+
+  var borderLogin = document.getElementsByClassName('login');
+
+  borderLogin[0].style = "border:none;";
+  border[0].style = "border-bottom: 3.5px solid red;";
+
+  register[0].style = "display:block;";
+  login[0].style = "display:none;";
+}
+
 // function popUp() {
 //    var popup = document.getElementById('popupBox');
 
